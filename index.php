6ebbe54b15ae9872e25fb6336fd39bd8c55dfd84
KarlisKalkis <kkalkis21@gmail.com>
2023-01-27 12:04:51 +0200

    Sakts veidot PHP

--- index.php
+++ index.php
@@ -1,11 +1,43 @@
 <?php
 //Using composer Class autoloader
-include "vendor/autoload.php";
+require_once __DIR__ . "vendor/autoload.php";
 
-use App\Database;
+//DB LOGIC
 
-$database= new Database;
+$userInput= $_SERVER[$REQUEST_URI];
 
-echo $database->host;
+$routes = [
+    '/'         => __DIR__. 'views/index.php',
+    '/sazina'   => __DIR__. 'views/sazina.php',
+    '/parmani'  => __DIR__. 'views/par_mani.php',
+    '/projekti' => __DIR__. 'views/projekti.php',
+];
+
+
+if (isset($routes[$userInput])){
+    include $routes[$userInput];
+
+}
+
+
+
+
+
+
+
+class User{
+    public function helloFromClass(){
+
+        echo'hello';
+    }
+}
+
+
+function helloUser()
+{
+    echo'hello user';
+}
+
+
 
 ?>
